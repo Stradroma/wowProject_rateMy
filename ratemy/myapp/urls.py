01a583694c076f789509bfb0b54dd5c8f71cb6de
curriejames <curriejames@icloud.com>
2021-12-20 23:56:37 -0500

    created templates boroughs added links between all pages added logo, counsel placeholder and select dropdown and basic styles

--- ratemy/myapp/urls.py
+++ ratemy/myapp/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('counselprofile', views.counselprofile, name='counselprofile'),
     path('addcounsel', views.addcounsel, name='addcounsel'),
     path('about', views.about, name='about'),
+    path('bronx', views.bronx, name='bronx'),
+    path('manhattan', views.manhattan, name='manhattan'),
+    path('queens', views.queens, name='queens'),
+    path('brooklyn', views.brooklyn, name='brooklyn'),
+    path('staten_island', views.staten_island, name='staten_island'),
 ]
